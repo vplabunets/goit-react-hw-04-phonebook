254cbac23cc4dc0c694d318f906d2583c211e39e
Volodymyr Labunets <vplabunets@gmail.com>
2022-10-30 21:04:18 +0200

    rework

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,25 @@
 import { React, useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
+import phonedata from '/projects/react/goit-react-hw-04-phonebook/src/constants/phonedata.json';
+import {
+  getlocalStorage,
+  setLocalStorage,
+} from '/projects/react/goit-react-hw-04-phonebook/src/utils/localStorage';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactsList';
 import { Filter } from '../Filter/Filter';
 import { AppWrap, PageTitle, SectionTitle } from './App.styled';
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(phonedata);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    if (!JSON.parse(localStorage.getItem('contacts'))) {
-      setContacts(JSON.parse(localStorage.getItem('contacts')));
-    }
-    window.localStorage.setItem('contactsData', JSON.stringify(contacts));
-    return;
+    getlocalStorage(contacts, setContacts, 'localStorageData');
+    setLocalStorage(contacts, 'localStorageData');
   }, [contacts]);
 
   const addContacts = contact => {
     const { name } = contact;
-    // const zzz = { id: nanoid(), ...contact };
     for (const contactCard of contacts) {
       if (contactCard.name === name) {
         return alert(`${name} is already contacts.`);
